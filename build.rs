31904213c82188d742e69ee5473d63bb41e295b8
Ankur Mallick <ankurmallick80@gmail.com>
2024-07-19 17:09:46 +0530

    update

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
+use std::fs;
+
 fn main() {
+    fs::copy("lib/audio.dll", "target/debug/audio.dll").unwrap();
+
     println!("cargo:rustc-link-lib=dylib=audio");
     println!("cargo:rustc-link-search=native=./lib");
 }
